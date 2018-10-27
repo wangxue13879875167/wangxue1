2d4ae9569584d6316ee6dc6d557fd7ac297f8cdd
王雪 <43264806+wangxue13879875167@users.noreply.github.com>
2018-10-27 11:28:17 +0800

    Add files via upload

---
+++ ex4-1.py
@@ -0,0 +1,11 @@
+K=5
+X=eval(input("请输入0~9之间的整数:"))
+tem=0
+while X!=K:
+    tem += 1
+    if (X>K):
+        print("遗憾，太大了")
+    else:
+        print("遗憾，太少了")
+    X=eval(input("请输入0~9之间的整数:"))
+print("预测{}次，你猜中了".format(tem))
